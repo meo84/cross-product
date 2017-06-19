333c0b8ef47f4ba735a02144353850a318e9f6cf
meo84 <oopsiday@gmail.com>
2017-06-19 16:32:36 +0200

    do a cross product of an array of values and an array of indices

--- lib/products.rb
+++ lib/products.rb
@@ -1,3 +1,11 @@
+def cross_product(values, indices)
+  raise ArgumentError, 'Arguments do not have the same size' unless values.size == indices.size
+
+  cross_product = []
+  indices.each { |index| cross_product << single_product(values, except_at: index) }
+  cross_product
+end
+
 def single_product(values, options = {})
   temp_values = values.dup
 
--- spec/products_spec.rb
+++ spec/products_spec.rb
@@ -1,6 +1,6 @@
 require 'products'
 
-describe 'Single product' do
+describe '.single_product' do
   it 'multiplies all values of an array' do
     values = [2, 10, 3, 5]
     expect(single_product(values)).to eq 2*10*3*5
@@ -10,5 +10,19 @@ describe 'Single product' do
     values = [2, 1, 3, 5]
     index = 1
     expect(single_product(values, except_at: index)).to eq 2*3*5
+  end
+end
+
+describe '.cross_product' do
+  it 'raises an error when arguments do not have the same size' do
+    values = [1]
+    indices = [1, 2]
+    expect { cross_product(values, indices) }.to raise_error(ArgumentError, 'Arguments do not have the same size')
+  end
+
+  it 'returns array of single products' do
+    values = [2, 1, 3, 5]
+    indices = [1, 2, 0, 3]
+    expect(cross_product(values, indices)).to eql [2*3*5, 2*1*5, 1*3*5, 2*1*3]
   end
 end
